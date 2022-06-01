776873bdde5bc7687119016e2f0b221450faa3e3
juan.gomez <gomezj01@stepstone.com>
2022-06-01 10:28:15 -0600

    Requiring HTTPS in ASP Net Core.

---
+++ ImplementingHTTPS/ImplementingHTTPS/Controllers/HelloWorldController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ImplementingHTTPS.Controllers
+{
+    [RequireHttps]
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
