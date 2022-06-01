1910c40affc3e15d5b0f8918f67d93aaa3806c4f
juan.gomez <gomezj01@stepstone.com>
2022-06-01 09:32:26 -0600

    Changed connection string

--- IdentityDemo/IdentityDemo/Program.cs
+++ IdentityDemo/IdentityDemo/Program.cs
@@ -1,5 +1,6 @@
 using IdentityDemo.Data;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Server.IISIntegration;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +14,8 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddRazorPages();
+
+builder.Services.AddAuthentication(IISDefaults.AuthenticationScheme);
 
 var app = builder.Build();
 
